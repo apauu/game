13694d12748aea220d153159bf532b52607bcf96
apauu <redbull.nomitai@gmail.com>
2014-02-08 14:38:37 +0900

    Stage01　当たり判定スクリプト

---
+++ survival_game/Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D collider) {
+		Debug.Log ("destoy!!");
+		}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Debug.Log ("HitHit");
+	}
+}
