3cccc13cf128d8f1f75587ddce247a714e166ad2
sisui <sainfrau1@gmail.com>
2014-02-08 14:40:05 +0900

    Stage02 プレイヤー移動スクリプト

---
+++ survival_game/Assets/Scene/Const.cs
@@ -0,0 +1,12 @@
+﻿
+public class Const {
+	/// <summary>
+	/// プレイヤー定数
+	/// </summary>
+	public const float PLAYER_SIDE_SPEED = 5f;
+	/// <summary>
+	/// プレイヤー定数
+	/// </summary>
+	public const float PLAYER_UP_SPEED = 1f;
+	public const string BBB = "1";
+}
---
+++ survival_game/Assets/Scene/Player_Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//左右ボタンの入力
+		float h = Input.GetAxisRaw ("Horizontal");
+		if (h > 0f) {
+			//右ボタンの時
+			rigidbody2D.AddForce (Vector2.right * Const.PLAYER_SIDE_SPEED);
+		} else if (h < 0f) {
+			//左ボタンの時
+			rigidbody2D.AddForce (Vector2.right * Const.PLAYER_SIDE_SPEED * -1);
+		} else {
+			//左右入力の無い時
+			//rigidbody2D.velocity.x = Vector2.right * 0;
+		}
+	}
+}
