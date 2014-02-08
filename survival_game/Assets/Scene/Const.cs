a40e395bb40525dd48b958a1a0d1d0c06f507f91
sisui <sainfrau1@gmail.com>
2014-02-08 17:37:07 +0900

    Stage02 itween

--- survival_game/Assets/Scene/Const.cs
+++ survival_game/Assets/Scene/Const.cs
@@ -8,5 +8,13 @@ public class Const {
 	/// プレイヤー定数
 	/// </summary>
 	public const float PLAYER_UP_SPEED = 1f;
+	/// <summary>
+	/// プレイヤー定数
+	/// </summary>
+	public const float DOUBLE_KEY_TIME = 2f;
+	/// <summary>
+	/// プレイヤー定数
+	/// </summary>
+	public const float PLAYER_DASH_SPEED = 15f;
 	public const string BBB = "1";
 }
