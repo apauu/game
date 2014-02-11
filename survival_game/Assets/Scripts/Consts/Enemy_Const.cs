ae4025f2e682bb13b153bb8e87b5d536e1f950f1
apauu <redbull.nomitai@gmail.com>
2014-02-11 17:59:55 +0900

    Enemyクラス追加

---
+++ survival_game/Assets/Scripts/Consts/Enemy_Const.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Const{
+	/// <summary>
+	/// エネミー定数
+	/// キャラクターとの距離　これ以上離れると元の位置に戻る
+	/// </summary>
+	public const float IMMOBILE_DISTANCE = 30f;
+	/// <summary>
+	/// エネミー定数
+	/// ベースとなる歩行スピード
+	/// </summary>
+	public const float ENEMY_SIDE_SPEED = 0.5f;
+}
