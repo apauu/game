a1febb5482f07a37a0c193dd3c78766409faec92
sisui <sainfrau1@gmail.com>
2014-04-20 21:25:10 +0900

    タイトル画面　画像追加

--- survival_game/Assets/Scripts/GUI/Title.cs
+++ survival_game/Assets/Scripts/GUI/Title.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class Title : MonoBehaviour {
 
 	public GameObject[] itemList;
+	public GameObject selectedIcon;
 	public float incX = 3;
 	public float incY = 3;
 
+	// 現在選択中のボタン番号
 	private int selected = 0;
 	private float key;
 	private bool selectableFlg = true;
@@ -39,7 +41,8 @@ public class Title : MonoBehaviour {
 
 		}
 	}
-	
+
+	// 選択中のボタン番号を変更する
 	void PlusSelected (int cnt) {
 		selected += cnt;
 		if (selected > itemList.Length - 1) {
@@ -49,20 +52,27 @@ public class Title : MonoBehaviour {
 		}
 	}
 
+	// 選択中のボタンに選択エフェクトを表示する
 	void SelectItem () {
-		Rect tmp = itemList[selected].guiTexture.pixelInset;
-		tmp.x -= incX;
-		tmp.y += incY;
-		itemList[selected].guiTexture.pixelInset = tmp;
+		//Rect tmp = itemList[selected].guiTexture.pixelInset;
+		//tmp.x -= incX;
+		//tmp.y += incY;
+		//itemList[selected].guiTexture.pixelInset = tmp;
+		Vector3 tmp = itemList[selected].transform.position;
+		Vector3 tmp2 = selectedIcon.transform.position;
+		tmp2.y = tmp.y;
+		selectedIcon.transform.position = tmp2;
 	}
 
+	// 選択中以外のボタンの選択エフェクトを終了する
 	void UnSelectItem () {
-		Rect tmp = itemList[selected].guiTexture.pixelInset;
-		tmp.x += incX;
-		tmp.y -= incY;
-		itemList[selected].guiTexture.pixelInset = tmp;
+		//Rect tmp = itemList[selected].guiTexture.pixelInset;
+		//tmp.x += incX;
+		//tmp.y -= incY;
+		//itemList[selected].guiTexture.pixelInset = tmp;
 	}
 
+	// 決定ボタン押下時の処理を行う
 	void EnterItem () {
 		Rect tmp = itemList[selected].guiTexture.pixelInset;
 		tmp.x += incX * 2;
