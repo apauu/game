3cfae6225b7abd08d72446ee27ca57c15b0f3acd
sisui <sainfrau1@gmail.com>
2014-05-17 14:30:28 +0900

    WIXOSS

---
+++ WIXOSS/Assets/script/Counter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Counter : MonoBehaviour {
+
+	public int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		guiText.text = count.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void next () {
+		count++;
+		guiText.text = count.ToString();
+	}
+
+	void prev () {
+		count--;
+		guiText.text = count.ToString();
+	}
+}
---
+++ WIXOSS/Assets/script/TextureRoll.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureRoll : MonoBehaviour {
+
+	public Texture[] textuerRoll; 
+	private int position = 0; 
+
+	// Use this for initialization
+	void Start () {
+		guiTexture.texture = textuerRoll [position];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void next () {
+		nextPosition ();
+		guiTexture.texture = textuerRoll [position];
+	}
+	
+	void prev () {
+		prevPosition ();
+		guiTexture.texture = textuerRoll [position];
+	}
+
+	private void nextPosition() {
+		position++;
+		if (position >= textuerRoll.Length) {
+			position = 0;
+		}
+	}
+	
+	private void prevPosition() {
+		position--;
+		if (position < 0) {
+			position = textuerRoll.Length;
+		}
+	}
+}
